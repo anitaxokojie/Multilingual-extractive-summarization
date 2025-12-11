b484966bb563acf5b729e027c87e8d49f9a479ff
Anita Okojie-Aidonojie <154951831+anitaxokojie@users.noreply.github.com>
2025-12-11 00:54:59 +0000

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,10 @@
-# Make the package importable
-from .models import BERTSummarizer, DomainTunedSummarizer
+"""
+TED Talk Summarization Package
+
+Provides extractive summarization using BERT embeddings and PageRank.
+"""
+
+from .models import BERTSummarizer, DomainTunedSummarizer, create_summarizer
+
+__version__ = "0.1.0"
+__all__ = ["BERTSummarizer", "DomainTunedSummarizer", "create_summarizer"]
